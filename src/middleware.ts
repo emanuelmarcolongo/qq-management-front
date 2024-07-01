609a2b31f5d5403150c6a04f0d08f1f87eec72e8
Emanuel Marcolongo <emanuelmarcolongo@gmail.com>
2024-07-01 18:39:27 -0300

    fix: correct path in middleware form /users to /app

--- src/middleware.ts
+++ src/middleware.ts
@@ -20,5 +20,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/dashboard/:path*", "/users/:path*"],
+  matcher: ["/dashboard/:path*", "/app/:path*"],
 };
